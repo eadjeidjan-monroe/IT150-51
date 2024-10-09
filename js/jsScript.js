858a6bc4b9c770d29b6d170e7b1de739f0a3355a
Eugene <eadjeidjan@hotmail.com>
2024-10-09 10:11:40 -0400

    javascript

---
+++ js/jsScript.js
@@ -0,0 +1,12 @@
+function myFirstFunction(){
+  document.getElementById("demo").innerHTML = "This is a text from the javascript function.";
+ }
+
+ function clearText(){
+  document.getElementById("demo").innerHTML = ""
+
+  function outTextByDocWrite(){
+    document.write("This is a text from the javascript function.")
+  }
+
+ }
